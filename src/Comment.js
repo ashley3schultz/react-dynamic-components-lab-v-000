99428ad875ae24387a6e05abb7cff13246239fab
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-19 22:13:00 +0000

    automatically backed up by learn

--- src/Comment.js
+++ src/Comment.js
@@ -5,7 +5,7 @@ export default class Comment extends Component {
   render() {
     return (
       <div className="comment">
-        {/* your conditional code here! */}
+        {this.props.commentText}
       </div>
     )
   }
